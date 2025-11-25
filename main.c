315bc751817ce6621ccbb603a7a02e09deeacbcc
lucasfratus <fratusdev@outlook.com>
2025-11-25 16:32:13 -0300

    implementa series do cosseno e seno

--- main.c
+++ main.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdint.h>
+#include <stdlib.h>
 
+
+#define k2  -0.50000000000000000000
+#define k3  -0.16666666666666666666
+#define k4   0.04166666666666666666
+#define k5   0.00833333333333333333
+
+#define k6  -1.388888888888889E-03
+#define k7  -1.984126984126984E-04
+#define k8   2.480158730158730E-05
+#define k9   2.755731922398589E-06
+
+#define k10  -2.755731922398589E-07
+#define k11  -2.505210838544172E-08
+#define k12   2.087675698786810E-09
+
+#define PI 3.14159265358979323846264
+#define PI4 (PI/4.0)
+#define PI2 (PI/2.0)
+#define dPI (PI*2.0)
 
 // Provavelmente não utilizaremos essa versão, já que não manipula via IEEE-754
 double pow_int(double base, int exp) {
@@ -65,4 +85,16 @@ double pow2_int(int k) {
     // sinal = 0, mantissa = 0
     // => valor = 1.0 * 2^(expo_guardado - bias) = 2^k
     return db.d;
+}
+
+// sen(x), |x| <= PI/4
+static double serie_seno(double x){
+    double y = x * x;
+    return x * (1 + y*(k3 + y*(k5 + y*(k7 + y*(k9 + y*k11)))));
+} 
+
+// cos(x), |x| <= PI/4
+static double serie_cos(double x){
+    double y = x * x;
+    return 1 + y* (k2 + y*(k4 + y*(k6 + y*(k8 + y*(k10 + y*k12)))));
 }
